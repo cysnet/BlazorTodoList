833b20cd9d0e9887d940c7c4ca5794996b611ecf
root <root@localhost.localdomain>
2022-05-19 18:31:16 +0800

    1

---
+++ SyncfusionTodoList/Data/TodoItemService.cs
@@ -0,0 +1,20 @@
+public class TodoItemService
+{
+    private List<TodoItem> data { get; set; } = new List<TodoItem> { new TodoItem { Name = "123" } };
+    public async Task<List<TodoItem>> Read()
+    {
+        return await Task.FromResult(data);
+    }
+
+    public async Task<bool> Create(TodoItem item)
+    {
+        data.Add(item);
+        return await Task.FromResult(true);
+    }
+
+    public async Task<bool> Delete(TodoItem item)
+    {
+        data.Remove(item);
+        return await Task.FromResult(true);
+    }
+}
